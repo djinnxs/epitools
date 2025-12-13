bfd59fe0d95846cfee26cba2b42784f283664065
djinnxs <33420901+djinnxs@users.noreply.github.com>
2025-12-13 13:29:02 -0300

    Change Streamlit app entry point to app.py

--- app.sh
+++ app.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Inicia la aplicación Streamlit y le dice que escuche en el puerto 7860
-streamlit run Home.py --server.address 0.0.0.0 --server.port 7860
+streamlit run app.py --server.address 0.0.0.0 --server.port 7860
